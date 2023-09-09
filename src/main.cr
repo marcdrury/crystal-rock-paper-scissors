111293b43fe0cf77dc1fbe3f53e341203ba57796
Marc Drury <marcdrury9@gmail.com>
2023-09-09 21:49:47 +0100

    feat: exit early if the user's input is invalid
    Exit early if the user's input isn't "rock", "paper", or "scissors".
    This is case insensitive.

--- src/main.cr
+++ src/main.cr
@@ -1,7 +1,13 @@
+valid_inputs = ["rock", "paper", "scissors"]
 puts "Rock, Paper, or Scissors?:"
 input = gets
 if input.nil? || input.empty?
   puts "No input. You must input \"rock\", \"paper\" or \"scissors\" when prompted."
+  exit(1)
+end
+input = input.downcase
+if valid_inputs.none? { |option| option == input }
+  puts "Invalid input. You must input \"rock\", \"paper\" or \"scissors\" when prompted."
   exit(1)
 end
 puts input
