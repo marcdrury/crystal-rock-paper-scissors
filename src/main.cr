d0b58a82c33766f86369adabcf88cbfb27718e20
Marc Drury <marcdrury9@gmail.com>
2023-09-12 20:51:06 +0100

    delete: remove message displaying the user's choice
    Temporary measure because the sanitised version of the user's input has
    been rendered inaccessible. A follow-up PR will expose it as a class
    variable of the PlayerChoice classes.

--- src/main.cr
+++ src/main.cr
@@ -3,6 +3,5 @@ require "./get-computer-choice"
 
 player_choice = PlayerChoiceGetter.get
 computer_choice = get_computer_choice
-puts "User chose #{sanitised_user_input}"
 puts "Computer chose #{computer_choice}"
 puts player_choice.determine_game_result computer_choice
