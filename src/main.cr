7261f508226833aede62d7479da8c78ac037d656
Marc Drury <marcdrury9@gmail.com>
2023-09-12 15:08:03 +0100

    refactor(main): pass the sanitised user input to the PlayerChoice factory

--- src/main.cr
+++ src/main.cr
@@ -5,14 +5,15 @@ require "./player-choice-factory"
 require "./get-computer-choice"
 
 user_input = get_user_input
+sanitised_user_input = nil
 begin
-  ChoiceParser.parse user_input
+  sanitised_user_input = ChoiceParser.parse user_input
 rescue exception : InvalidInputException
   puts exception.message
   exit(1)
 end
-player_choice = player_choice_factory user_input
+player_choice = player_choice_factory sanitised_user_input
 computer_choice = get_computer_choice
-puts "User chose #{user_input}"
+puts "User chose #{sanitised_user_input}"
 puts "Computer chose #{computer_choice}"
 puts player_choice.determine_game_result computer_choice
