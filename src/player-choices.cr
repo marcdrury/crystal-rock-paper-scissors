392ed7ba4ea135cabc80cf4496fa7a9f738f8e6a
Marc Drury <marcdrury9@gmail.com>
2023-09-12 21:14:02 +0100

    feat(player-choices): add a class variable which identifies player choice

--- src/player-choices.cr
+++ src/player-choices.cr
@@ -1,6 +1,7 @@
 abstract class PlayerChoice
   @@WINS_AGAINST = ""
   @@LOSES_AGAINST = ""
+  @@IS = ""
 
   def determine_game_result(opponent_choice : String) : String
     if opponent_choice == @@WINS_AGAINST
@@ -16,14 +17,17 @@ end
 class Rock < PlayerChoice
   @@WINS_AGAINST = "scissors"
   @@LOSES_AGAINST = "paper"
+  @@IS = "rock"
 end  
 
 class Paper < PlayerChoice
   @@WINS_AGAINST = "rock"
   @@LOSES_AGAINST = "scissors"
+  @@IS = "paper"
 end
 
 class Scissors < PlayerChoice
   @@WINS_AGAINST = "paper"
   @@LOSES_AGAINST = "rock"
+  @@IS = "scissors"
 end
