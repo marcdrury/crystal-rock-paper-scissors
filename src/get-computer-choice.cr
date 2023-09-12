a3b500274e927f8498d51f999a85d31bdd7d7fd8
Marc Drury <marcdrury9@gmail.com>
2023-09-12 12:15:41 +0100

    refactor: use VALID_OPTIONS constant

--- src/get-computer-choice.cr
+++ src/get-computer-choice.cr
@@ -1,5 +1,6 @@
+require "./valid-options"
+
 def get_computer_choice : String
-  valid_options = ["rock", "paper", "scissors"]
   random_index = Random.new.rand(3)
-  valid_options[random_index]
+  VALID_OPTIONS[random_index]
 end
--- src/input-validator.cr
+++ src/input-validator.cr
@@ -1,4 +1,5 @@
 require "./invalid-input-exception"
+require "./valid-options"
 
 module InputValidator
   extend self
@@ -17,9 +18,8 @@ module InputValidator
   end
 
   private def check_input_valid_option(input : String) : Nil
-    valid_inputs = ["rock", "paper", "scissors"]
     input_lowercased = input.downcase
-    if valid_inputs.none? { |option| option == input_lowercased }
+    if VALID_OPTIONS.none? { |option| option == input_lowercased }
       error_message = "Invalid input. You must input \"rock\", \"paper\" or \"scissors\" when prompted."
       raise InvalidInputException.new error_message
     end
