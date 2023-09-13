7485e3512a5d5439e0ee36de3106079d2abf2e63
Marc Drury <marcdrury9@gmail.com>
2023-09-13 20:44:04 +0100

    feat(PlayerChoice): add colour to round result messages

--- src/player-choices.cr
+++ src/player-choices.cr
@@ -22,7 +22,18 @@ abstract class PlayerChoice
   private def display_round_info(opponent_choice : String, result : String) : Nil
     puts "You chose: #{@@IS.capitalize}"
     puts "Opponent chose: #{opponent_choice.capitalize}"
-    puts result == "draw" ? "It's a draw!" : "You #{result}!"
+    self.display_round_result result
+  end
+
+  private def display_round_result(result : String) : Nil
+    case result
+    when "draw"
+      puts "It's a draw!".colorize(:yellow)
+    when "win"
+      puts "You win!".colorize(:green)
+    when "lose"
+      puts "You lose!".colorize(:red)
+    end
   end
 
   private def game_over?(result : String) : Bool
